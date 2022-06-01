270f041778f363f075230f1a96988dd2755d7c8b
Ikhsan-Agil <ikhsankusuma97@gmail.com>
2022-06-01 12:07:32 +0700

    Ikhsan Agil Kusuma - Seventh Commit - add root routes for deployment testing

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const dbconnect = require("./config/dbconnect");
+const dbconnect = require('./config/dbconnect');
 const authRouter = require('./routes/authRouter.js');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
 //retrieve and load .env file
-dotenv.config({path: "./config/config.env"})
+dotenv.config({path: './config/config.env'});
 
 //connect to MongoDB
-dbconnect()
+dbconnect();
 
 const app = express();
 
 //body parser
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 
 //logging
-app.use(morgan("dev"))
+app.use(morgan('dev'));
 
-app.use((error, req, res, next)=> {
-  const statusCode = 200 ? 500 : req.statusCode
-  res.status(statusCode)
-  res.json({message: error.message})
-})
-
+app.use((error, req, res, next) => {
+  const statusCode = 200 ? 500 : req.statusCode;
+  res.status(statusCode);
+  res.json({message: error.message});
+});
 
 //routes
 app.use('/', authRouter);
 
-//For security, please write port number in .env file
-const port = process.env.PORT || process.env.PORT_ALTERNATIVE
-app.listen(port, console.log(`Server is running on Port: http://localhost:${port}`));
+// for deployment testing, please delete after testing is success.
+app.get('/', (req, res) => {
+  res.send('SuperScan Server is running');
+});
 
+//For security, please write port number in .env file
+const port = process.env.PORT || process.env.PORT_ALTERNATIVE;
+app.listen(
+  port,
+  console.log(`Server is running on Port: http://localhost:${port}`)
+);
