9ee620116a49c67a82dc8ce117719af0a1f33f42
Ikhsan-Agil <ikhsankusuma97@gmail.com>
2022-06-08 23:20:55 +0700

    Ikhsan Agil Kusuma - upgrade the logic for fetch data, add a order logic, and add /getProduct url at documentation file

--- routes/rateRouter.js
+++ routes/rateRouter.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const {rating} = require("../controller/ratingController");
-const {verifyToken} = require("../middleware/jsonwebtoken");
+const express = require('express');
+const {rating} = require("../controller/ratingController")
+const {verifyToken} = require("../middleware/jsonwebtoken")
 // const validator = require('validator');
 const rateRouter = express.Router();
 
-rateRouter.route("/rate", rating);
+rateRouter.route('/rate').post(verifyToken, rating)
 
-module.exports = rateRouter;
+module.exports = rateRouter
